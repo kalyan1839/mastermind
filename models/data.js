5e2a87e90a5782080f6ec288ffd0e850219fb46c
Kalyan Y <yedakulakalyan@gmail.com>
2016-11-24 01:53:23 +0530

    https

--- models/data.js
+++ models/data.js
@@ -8,7 +8,7 @@ app.factory('Football', ['$http', function($http){
 	Football.prototype = {
 		competitions: function(){
 			var scope = this;
-			return $http.get('http://api.football-data.org/v1/competitions/?season=2016', { headers: {'Authorization': '0a5b1343569648638cddb1e8ffb5128a'}});
+			return $http.get('https://api.football-data.org/v1/competitions/?season=2016', { headers: {'Authorization': '0a5b1343569648638cddb1e8ffb5128a'}});
 		}
 	}
 
